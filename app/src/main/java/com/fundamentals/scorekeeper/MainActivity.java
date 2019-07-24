9fc3dc3a9149ec53dd2f7a068825dfc14e995845
Christos Christidis <christos.christidis86@gmail.com>
2019-07-24 21:09:44 +0300

    Drawables, styles, themes

---
+++ app/src/main/java/com/fundamentals/scorekeeper/MainActivity.java
@@ -0,0 +1,96 @@
+package com.fundamentals.scorekeeper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String STATE_SCORE_1 = "Team 1 Score";
+    private static final String STATE_SCORE_2 = "Team 2 Score";
+
+    private TextView mScoreText1;
+    private TextView mScoreText2;
+
+    private int mScore1;
+    private int mScore2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mScoreText1 = findViewById(R.id.score_1);
+        mScoreText2 = findViewById(R.id.score_2);
+
+        if (savedInstanceState != null) {
+            mScore1 = savedInstanceState.getInt(STATE_SCORE_1);
+            mScore2 = savedInstanceState.getInt(STATE_SCORE_2);
+
+            mScoreText1.setText(String.valueOf(mScore1));
+            mScoreText2.setText(String.valueOf(mScore2));
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(STATE_SCORE_1, mScore1);
+        outState.putInt(STATE_SCORE_2, mScore2);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        int nightMode = AppCompatDelegate.getDefaultNightMode();
+        if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+            menu.findItem(R.id.night_mode).setTitle(R.string.day_mode);
+        } else {
+            menu.findItem(R.id.night_mode).setTitle(R.string.night_mode);
+        }
+        return true;
+    }
+
+    // SOS: To change the theme to night_mode, activity must be destroyed & recreated
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.night_mode) {
+            int nightMode = AppCompatDelegate.getDefaultNightMode();
+            if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+        }
+        recreate();
+        return true;
+    }
+
+    public void increaseScore(View view) {
+        switch (view.getId()) {
+            case R.id.increaseTeam1:
+                mScore1++;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+            case R.id.increaseTeam2:
+                mScore2++;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
+    }
+
+    public void decreaseScore(View view) {
+        switch (view.getId()) {
+            case R.id.decreaseTeam1:
+                mScore1--;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+            case R.id.decreaseTeam2:
+                mScore2--;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
+    }
+}
